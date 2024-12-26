0befd1b60ee1feadf973be296230e961b648ca71
Ebrar Kaya <ebrkaya9852@gmail.com>
2024-12-26 16:57:44 +0300

    Update soru8.py

--- soru8.py
+++ soru8.py
@@ -1,26 +1,21 @@
-# Başlangıç stokları
 elma = 50
 portakal = 30
 muz = 20
 
 while True:
-    # Başlangıç stoklarını ekrana yazdır
-    print("\nElma stoku: {}".format(elma))
+    print("Elma stoku: {}".format(elma))
     print("Portakal stoku: {}".format(portakal))
     print("Muz stoku: {}".format(muz))
-
-    # Kullanıcıdan ürün seçmesini iste
     sec = input("Lütfen satın almak istediğiniz ürünü giriniz (Elma, Portakal, Muz). Çıkmak için 'x' tuşuna basın: ").lower()
 
-    if sec == 'x':  # Çıkmak için 'x' tuşuna basılacak
+    if sec == 'x':
         print("Çıkılıyor...")
         break
     
-    # Ürün seçimine göre stok miktarını kontrol et ve işlem yap
     if sec == 'elma':
         es = int(input("Satın almak istediğiniz elma sayısını giriniz: "))
         if es <= elma:
-            elma -= es  # Stoktan azalt
+            elma -= es 
             print("Yeni Elma stoku: {}".format(elma))
         else:
             print("Yetersiz stok! Elma stok miktarı: {}".format(elma))
@@ -28,7 +23,7 @@ while True:
     elif sec == 'portakal':
         es = int(input("Satın almak istediğiniz portakal sayısını giriniz: "))
         if es <= portakal:
-            portakal -= es  # Stoktan azalt
+            portakal -= es 
             print("Yeni Portakal stoku: {}".format(portakal))
         else:
             print("Yetersiz stok! Portakal stok miktarı: {}".format(portakal))
@@ -36,7 +31,7 @@ while True:
     elif sec == 'muz':
         es = int(input("Satın almak istediğiniz muz sayısını giriniz: "))
         if es <= muz:
-            muz -= es  # Stoktan azalt
+            muz -= es
             print("Yeni Muz stoku: {}".format(muz))
         else:
             print("Yetersiz stok! Muz stok miktarı: {}".format(muz))
